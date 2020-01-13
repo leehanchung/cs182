b7296961f6abe72cf6ed613a764031d0c4df87b8
Han <kein520@yahoo.com>
2020-01-12 21:51:46 -0800

    dqn and ddqn done. video recorder issue on atari

---
+++ assignment4/batch_train_ddqn_cartpole.sh
@@ -0,0 +1,5 @@
+python train_dqn.py CartPole-v0 --num_steps 100000 --double_q
+python train_dqn.py CartPole-v0 --num_steps 100000 --double_q
+python train_dqn.py CartPole-v0 --num_steps 100000 --double_q
+python train_dqn.py CartPole-v0 --num_steps 100000 --double_q
+python train_dqn.py CartPole-v0 --num_steps 100000 --double_q
